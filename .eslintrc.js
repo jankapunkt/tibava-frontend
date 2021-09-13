dba6cb419c0aee22dea6cd78f768c61915d28122
ghauri <junaidghauri205@gmail.com>
2021-09-13 17:47:04 +0200

    resolve eslint related issue so can compile/deploy on different machine easily

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "parser": "babel-eslint",
+    "extends": "plugin:vue/essential",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
