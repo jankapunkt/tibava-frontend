88a75be9cb37accb6bb063508f9ab1b03319d7b8
e-mb <eric_mueller89@hotmail.com>
2022-05-31 14:53:01 +0200

    improved annotation visualization including max text length and tooltips

--- src/components/CurrentEntitiesOverView.vue
+++ src/components/CurrentEntitiesOverView.vue
@@ -1,23 +1,44 @@
 <template>
   <v-row align="center" justify="center" class="px-2 py-2">
-    <v-col cols="10">
-      <v-item-group>
-        <v-row v-for="(item, i) in annotations" :key="i">
-          <v-col>
-            <v-progress-linear
-              :color="item.color"
+    <v-col cols="12">
+      <div class="pa-4">
+        <!-- <v-chip-group column>
+          <v-chip v-for="(item, i) in annotations" :key="i">
+            <v-progress-circular
               color="#d99090"
-              rounded
-              height="70"
               :value="((time - item.start) / (item.end - item.start)) * 100"
-            >
-              <template v-slot:default="{ name }">
-                <strong class="mx-2">{{ item.entity_name }}</strong>
-              </template>
-            </v-progress-linear>
-          </v-col>
-        </v-row>
-      </v-item-group>
+            ></v-progress-circular>
+            <strong class="mx-2">{{ item.entity_name }}</strong>
+          </v-chip>
+        </v-chip-group> -->
+
+        <div v-for="(item, i) in annotations" :key="i" class="my-2">
+          <v-progress-linear
+            :color="item.color"
+            color="#d99090"
+            height="30"
+            rounded
+            :value="((time - item.start) / (item.end - item.start)) * 100"
+          >
+            <v-tooltip top>
+              <template v-slot:activator="{ on, attrs }">
+                <span
+                  v-bind="attrs"
+                  v-on="on"
+                  class="mx-2"
+                  style="
+                    overflow: hidden;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                  "
+                  >{{ item.entity_name }}</span
+                ></template
+              >
+              <span>{{ item.entity_name }}</span>
+            </v-tooltip>
+          </v-progress-linear>
+        </div>
+      </div>
     </v-col>
   </v-row>
 </template>
