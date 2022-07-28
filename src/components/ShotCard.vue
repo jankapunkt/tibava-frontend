8909a49de7fa0277a5fe69781ad3b5faac5796a6
Matthias Springstein <matthias.springstein@gmail.com>
2022-07-28 14:36:29 +0200

    Fix seeking when click on a shot in shot overview.

--- src/components/ShotCard.vue
+++ src/components/ShotCard.vue
@@ -1,25 +1,15 @@
 <template>
-  <v-card
-    class="d-flex flex-column pa-2 ma-4"
-    elevation="4"
-    v-on:click="setVideoPlayerTime(shot.start)"
-  >
+  <v-card class="d-flex flex-column pa-2 ma-4" elevation="4" v-on:click="setVideoPlayerTime(shot.start)">
     <v-card-title class="px-2 py-1">Shot {{ shot.id }}</v-card-title>
     <v-row align="center" justify="center" class="px-2 py-0">
       <v-col cols="4">
         <v-list-item three-line>
           <v-list-item-content min-width>
             <!-- <div class="text-overline mb-4">Shot {{ shot.id }}</div> -->
-            <v-list-item-subtitle
-              >Begin: {{ get_timecode(shot.start) }}</v-list-item-subtitle
-            >
-            <v-list-item-subtitle
-              >End: {{ get_timecode(shot.end) }}</v-list-item-subtitle
-            >
-            <v-list-item-subtitle
-              >Duration:
-              {{ get_timecode(shot.end - shot.start) }}</v-list-item-subtitle
-            >
+            <v-list-item-subtitle>Begin: {{ get_timecode(shot.start) }}</v-list-item-subtitle>
+            <v-list-item-subtitle>End: {{ get_timecode(shot.end) }}</v-list-item-subtitle>
+            <v-list-item-subtitle>Duration:
+              {{ get_timecode(shot.end - shot.start) }}</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-col>
@@ -40,13 +30,19 @@
 <script>
 import TimeMixin from "../mixins/time";
 
+import { mapStores } from "pinia";
+import { usePlayerStore } from "@/store/player";
+
 export default {
   mixins: [TimeMixin],
   props: ["shot"],
   methods: {
     setVideoPlayerTime(time) {
-      this.$emit("seek", time);
+      this.playerStore.setTargetTime(time);
     },
   },
+  computed: {
+    ...mapStores(usePlayerStore),
+  }
 };
 </script>
--- src/components/ShotsOverview.vue
+++ src/components/ShotsOverview.vue
@@ -3,7 +3,7 @@
     <v-virtual-scroll :items="shots" height="500" item-height="160">
       <template v-slot:default="{ item }">
         <v-list-item :key="item.id">
-          <ShotCard :shot="item" v-on:seek="setVideoPlayerTime" />
+          <ShotCard :shot="item" />
         </v-list-item>
       </template>
     </v-virtual-scroll>
@@ -14,12 +14,6 @@
 import ShotCard from "@/components/ShotCard.vue";
 export default {
   props: ["shots"],
-  methods: {
-    setVideoPlayerTime(time) {
-      this.$emit("seek", time);
-    },
-  },
-  computed: {},
   components: {
     ShotCard,
   },
