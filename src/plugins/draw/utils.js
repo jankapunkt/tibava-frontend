83f97704d7300ef3bad5f86d566ead3707797377
eric-mb <e-mb@tutanota.com>
2022-08-10 12:36:50 +0200

    d3 colormaps

--- src/plugins/draw/utils.js
+++ src/plugins/draw/utils.js
@@ -1,64 +1,77 @@
 import * as PIXI from "pixi.js";
-import palette from "google-palette";
+import * as d3 from "d3";
 
 // import { magma, seismic, jet } from "./palette.js";
 
-var colors = palette("tol-dv", 101);
+var colormaps = new Map([
+  ["RdYlBu", d3.interpolateRdYlBu],
+  ["RdYlGn", d3.interpolateRdYlGn],
+  ["Blues", d3.interpolateBlues],
+  ["Greens", d3.interpolateGreens],
+  ["Reds", d3.interpolateReds],
+  ["Greys", d3.interpolateGreys],
+  ["YlGnBu", d3.interpolateYlGnBu],
+  ["YlOrRd", d3.interpolateYlOrRd],
+]);
 
 export function hex2luminance(string) {
-    const rgb = PIXI.utils.hex2rgb(string);
-    return Math.sqrt(
-        0.299 * Math.pow(rgb[0], 2) +
-        0.587 * Math.pow(rgb[1], 2) +
-        0.114 * Math.pow(rgb[2], 2)
-    );
+  const rgb = PIXI.utils.hex2rgb(string);
+  return Math.sqrt(
+    0.299 * Math.pow(rgb[0], 2) +
+      0.587 * Math.pow(rgb[1], 2) +
+      0.114 * Math.pow(rgb[2], 2)
+  );
 }
 
 export function linspace(startValue, stopValue, cardinality) {
-    var arr = [];
-    var step = (stopValue - startValue) / (cardinality - 1);
-    for (var i = 0; i < cardinality; i++) {
-        arr.push(startValue + step * i);
-    }
-    return arr;
+  var arr = [];
+  var step = (stopValue - startValue) / (cardinality - 1);
+  for (var i = 0; i < cardinality; i++) {
+    arr.push(startValue + step * i);
+  }
+  return arr;
 }
 
-export function scalarToHex(s, invert = false) {
-    // maps a scalar [0, 1] to a color value
-    if (invert) {
-        s = 1 - s;
-    }
-    return PIXI.utils.string2hex(colors[Math.round(s * 100)]);
+export function scalarToHex(s, invert = false, colorPalette = "Reds") {
+  // maps a scalar [0, 1] to a color value
+  if (invert) {
+    s = 1 - s;
+  }
+
+  const cm = colormaps.get(colorPalette);
+  var color = cm(s);
+  //   var color = d3.interpolateRdYlGn(s);
+  return PIXI.utils.string2hex(d3.color(color).formatHex());
 }
 
 export function resampleApprox({ data, targetSize = 1024 }) {
-    const stepsize = 2 ** Math.max(Math.ceil(Math.log2(data.length) - Math.log2(targetSize)), 0)
-    const filteredData = data.filter((e, i) => i % stepsize == 0)
-    return filteredData
+  const stepsize =
+    2 ** Math.max(Math.ceil(Math.log2(data.length) - Math.log2(targetSize)), 0);
+  const filteredData = data.filter((e, i) => i % stepsize == 0);
+  return filteredData;
 }
 
 export function generateFont() {
-
-    PIXI.BitmapFont.from(
-        "default_font",
-        {
-            fill: "#333333",
-            fontSize: 10,
-        },
-        {
-            chars: [["a", "z"], ["0", "9"], ["A", "Z"], " \\|/:.-^%$&*()!?"],
-            // fontWeight: 'bold',
-        }
-    );
-    PIXI.BitmapFont.from(
-        "default_white_font",
-        {
-            fill: "#FFFFFF",
-            fontSize: 10,
-        },
-        {
-            chars: [["a", "z"], ["0", "9"], ["A", "Z"], " \\|/:.-^%$&*()!?"],
-            // fontWeight: 'bold',
-        }
-    );
+  PIXI.BitmapFont.from(
+    "default_font",
+    {
+      fill: "#333333",
+      fontSize: 10,
+    },
+    {
+      chars: [["a", "z"], ["0", "9"], ["A", "Z"], " \\|/:.-^%$&*()!?"],
+      // fontWeight: 'bold',
+    }
+  );
+  PIXI.BitmapFont.from(
+    "default_white_font",
+    {
+      fill: "#FFFFFF",
+      fontSize: 10,
+    },
+    {
+      chars: [["a", "z"], ["0", "9"], ["A", "Z"], " \\|/:.-^%$&*()!?"],
+      // fontWeight: 'bold',
+    }
+  );
 }
