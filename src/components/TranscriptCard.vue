1c3aa2314c6d7fe0cc67c8a38cc26b113fc6d2c8
Christian Otto Home_Office_Desktop <chro@outlook.de>
2023-06-08 14:27:02 +0200

    added transcript highlighting depending on video player timestamp

--- src/components/TranscriptCard.vue
+++ src/components/TranscriptCard.vue
@@ -1,8 +1,14 @@
 <template>
-  <v-card class="d-flex flex-column pa-2 ma-4" elevation="4" v-on:click="setVideoPlayerTime(transcript.start)">
-    <span style="color:rgb(0, 0, 0); margin-bottom:0.2cm">{{ get_timecode(transcript.start) }}</span>
-    <span>{{transcript.name}}</span>
-  </v-card>
+  <div>
+    <v-card
+    :class="['d-flex', 'flex-column', 'pa-2', 'ma-4', { highlighted: isHighlighted }]"
+    elevation="4"
+    v-on:click="setVideoPlayerTime(transcript.start)"
+    >
+      <span style="color:rgb(0, 0, 0); margin-bottom:0.2cm">{{ get_timecode(transcript.start) }}</span>
+      <span>{{transcript.name}}</span>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -10,10 +16,7 @@ import TimeMixin from "../mixins/time";
 
 import { mapStores } from "pinia";
 import { usePlayerStore } from "@/store/player";
-import { useAnnotationStore } from "../store/annotation";
-import { useAnnotationCategoryStore } from "../store/annotation_category";
 import { useTimelineSegmentAnnotationStore } from "../store/timeline_segment_annotation";
-import { useTimelineSegmentStore } from "../store/timeline_segment";
 
 export default {
   mixins: [TimeMixin],
@@ -24,27 +27,20 @@ export default {
     },
   },
   computed: {
-    annotations(){
+    isHighlighted() {
       const cur_time = this.playerStore.currentTime;
-      const timeline_segment_annotations = this.timelineSegmentAnnotationStore.forTimeLUT(cur_time)
-      return timeline_segment_annotations.map((e) => {
-        const timeline_segment = this.timelineSegmentStore.get(e.timeline_segment_id);
-        const annotation = this.annotationStore.get(e.annotation_id);
-        let cat = null;
-        if("category_id" in annotation){
-          cat = this.annotationCategoryStore.get(annotation.category_id)
-        }
-        else
-        {
-          cat = "Unknown"
-        }
-        return { name: annotation.name, color:annotation.color, id:annotation.id, category: cat, start:timeline_segment.start, end:timeline_segment.end }
-     })
+      return this.transcript.start <= cur_time && this.transcript.end > cur_time;
     },
     time(){
       return this.playerStore.currentTime;
     },
-    ...mapStores(usePlayerStore, useAnnotationCategoryStore, useTimelineSegmentStore, useAnnotationStore, useTimelineSegmentAnnotationStore),
+    ...mapStores(usePlayerStore, useTimelineSegmentAnnotationStore),
   }
 };
 </script>
+
+<style>
+  .highlighted {
+    background-color: rgba(43, 24, 27, 0.287) !important
+  }
+</style> 
