3ded3cfacd38fd707787d10b7aeb85bf48b34ba0
matthias springstein <matthias.springstein@gmail.com>
2024-03-19 10:47:31 +0100

    Use a annotation timeline for shots overview if no shotdetection is available.

--- src/store/shot.js
+++ src/store/shot.js
@@ -3,13 +3,18 @@ import { defineStore } from "pinia";
 
 import { usePlayerStore } from "./player";
 import { usePluginRunStore } from "./plugin_run";
+import { useTimelineStore } from "./timeline";
+import { useTimelineSegmentStore } from "./timeline_segment";
 import { usePluginRunResultStore } from "./plugin_run_result";
 
 export const useShotStore = defineStore("shot", {
   getters: {
-    shots () {
+    shots() {
+
       const pluginRunStore = usePluginRunStore();
       const pluginRunResultStore = usePluginRunResultStore();
+      const timelineStore = useTimelineStore();
+      const timelineSegmentStore = useTimelineSegmentStore();
       const playerStore = usePlayerStore();
 
       // selection of timeline to be used for the thumbnails
@@ -25,10 +30,53 @@ export const useShotStore = defineStore("shot", {
         .sort((a, b) => {
           return new Date(b.date) - new Date(a.date);
         });
+
       if (!shotdetection.length) {
+        console.error("Shots: No shotdetection run")
         return [];
       }
+
       shotdetection = shotdetection.at(-1); // use latest shotdetection
+
+      let results = [];
+
+
+      // Something is wrong here
+      if (!shotdetection.results.length) {
+
+        console.error("Shots: No shotdetection run results")
+        let timeline = timelineStore
+          .forVideo(playerStore.videoId)
+          .filter((e) => {
+            return e.type == "ANNOTATION";
+          })
+
+        if (!timeline.length) {
+          console.error("Shots: No annotation timeline")
+          return results
+        }
+        results = timelineSegmentStore.forTimeline(timeline.at(0).id).map((e) => {
+          return {
+            start: e.start,
+            end: e.end,
+          };
+        })
+      }
+      else {
+        // get start and end times for each shot
+        if (
+          "results" in shotdetection &&
+          shotdetection.results.length > 0 &&
+          "data" in shotdetection.results[0]
+        ) {
+          results = shotdetection.results[0].data.shots.map((e) => {
+            return {
+              start: e.start,
+              end: e.end,
+            };
+          });
+        }
+      }
 
       // selection of thumbnails to be used
       let thumbnail = pluginRunStore
@@ -45,24 +93,10 @@ export const useShotStore = defineStore("shot", {
         });
 
       if (!thumbnail.length) {
-        return results;
+        console.error("Shots: No thumbnail run")
+        return [];
       }
       thumbnail = thumbnail.at(0); // use latest thumbnails
-
-      // get start and end times for each shot
-      let results = [];
-      if (
-        "results" in shotdetection &&
-        shotdetection.results.length > 0 &&
-        "data" in shotdetection.results[0]
-      ) {
-        results = shotdetection.results[0].data.shots.map((e) => {
-          return {
-            start: e.start,
-            end: e.end,
-          };
-        });
-      }
 
       // create thumbnail dict indicating the thumbnail for a specific time
       let thumbnail_list = [];
@@ -89,13 +123,13 @@ export const useShotStore = defineStore("shot", {
           end: e.end,
           thumbnails: [
             thumbnail_list[
-              Math.min(
-                Math.ceil(e.start / delta_time),
-                thumbnail_list.length - 1
-              )
+            Math.min(
+              Math.ceil(e.start / delta_time),
+              thumbnail_list.length - 1
+            )
             ],
             thumbnail_list[
-              Math.round((e.start + (e.end - e.start) / 2) / delta_time)
+            Math.round((e.start + (e.end - e.start) / 2) / delta_time)
             ],
             thumbnail_list[Math.floor(e.end / delta_time)],
           ],
@@ -103,7 +137,7 @@ export const useShotStore = defineStore("shot", {
       });
 
       return results;
-      }
     }
-  },
+  }
+},
 );
