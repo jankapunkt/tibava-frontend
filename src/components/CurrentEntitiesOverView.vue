a5aec68c9112053dcfd8f482a2a12ece4ab1ada4
matthias springstein <matthias.springstein@gmail.com>
2024-06-03 13:25:06 +0200

    Add timeline name in front of the annotation in current entities overview.

--- src/components/CurrentEntitiesOverView.vue
+++ src/components/CurrentEntitiesOverView.vue
@@ -3,27 +3,16 @@
     <v-col cols="12">
       <div class="pa-4">
         <div v-for="(item, i) in current_annotations" :key="i" class="my-2">
-          <v-progress-linear
-            :color="item.color"
-            height="30"
-            rounded
-            :value="((time - item.start) / (item.end - item.start)) * 100"
-          >
+          <v-progress-linear :color="item.color" height="30" rounded
+            :value="((time - item.start) / (item.end - item.start)) * 100">
             <v-tooltip top>
               <template v-slot:activator="{ on, attrs }">
-                <span
-                  v-bind="attrs"
-                  v-on="on"
-                  class="mx-2"
-                  style="
+                <span v-bind="attrs" v-on="on" class="mx-2" style="
                     overflow: hidden;
                     white-space: nowrap;
                     text-overflow: ellipsis;
-                  "
-                  >{{ item.name }}</span
-                ></template
-              >
-              <span>{{ item.name }}</span>
+                  ">{{ item.timeline_name }} :: {{ item.name }}</span></template>
+              <span>{{ item.timeline_name }} :: {{ item.name }}</span>
             </v-tooltip>
           </v-progress-linear>
         </div>
@@ -41,32 +30,34 @@ import { usePlayerStore } from "../store/player";
 import { useTimelineSegmentAnnotationStore } from "../store/timeline_segment_annotation";
 import { useAnnotationStore } from "../store/annotation";
 import { useTimelineSegmentStore } from "../store/timeline_segment";
+import { useTimelineStore } from "../store/timeline";
 
 
 
 export default {
   mixins: [TimeMixin],
-  computed:{
+  computed: {
     annotations() {
       const annotationsMap = {};
       this.timelineSegmentAnnotationStore.all.forEach(annotation => {
         const timelineSegment = this.timelineSegmentStore.get(annotation.timeline_segment_id);
         const annotationData = this.annotationStore.get(annotation.annotation_id);
-        
+
         annotationsMap[annotation.id] = {
           name: annotationData ? annotationData.name : null,
+          timeline_name: this.timelineStore.get(timelineSegment.timeline_id).name,
           color: annotationData ? annotationData.color : "white",
           id: annotation.id,
           start: timelineSegment ? timelineSegment.start : 0,
           end: timelineSegment ? timelineSegment.end : 0
         };
       });
-      
+
       return annotationsMap;
     },
     annotationsByTime() {
       let lut = {};
-      for (let t=0; t<Math.ceil(this.playerStore.videoDuration); t++) {
+      for (let t = 0; t < Math.ceil(this.playerStore.videoDuration); t++) {
         lut[t] = [];
         for (const annotation of Object.values(this.annotations)) {
           if (annotation.start <= t && t < annotation.end) {
@@ -76,14 +67,14 @@ export default {
       }
       return lut;
     },
-    current_annotations(){
+    current_annotations() {
       const time = Math.round(this.time);
       return this.annotationsByTime[time].map((id) => this.annotations[id]);
     },
-    time(){
+    time() {
       return this.playerStore.currentTime;
     },
-    ...mapStores(usePlayerStore, useAnnotationStore, useTimelineSegmentAnnotationStore, useTimelineSegmentStore),
+    ...mapStores(usePlayerStore, useAnnotationStore, useTimelineStore, useTimelineSegmentAnnotationStore, useTimelineSegmentStore),
   }
 };
 </script>
